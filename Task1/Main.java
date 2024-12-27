c2d308bc5d43fa63db94d240f755c0cdf193ee7f
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 13:39:58 +0300

    Create Main.java

---
+++ Task1/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(25, "Света"));
+        people.add(new Person(30, "Витя"));
+        people.add(new Person(25, "Петя"));
+        people.add(new Person(30, "Ваня"));
+        people.add(new Person(20, "Лена"));
+
+        System.out.println("Список людей до сортировки:");
+        for (Person person : people) {
+            System.out.println(person.getName() + ", " + person.getAge());
+        }
+
+        // Сортируем список по возрасту и имени
+        Collections.sort(people, new PersonSuperComparator());
+
+        System.out.println("\nСписок людей после сортировки:");
+        for (Person person : people) {
+            System.out.println(person.getName() + ", " + person.getAge());
+        }
+    }
+}
