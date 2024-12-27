7dce247c55b3d77e9c9b4091b59bcc9efe52fbee
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 14:55:03 +0300

    Create Main.java

---
+++ Task4/Main.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        EvenLengthRemover remover = new EvenLengthRemover();
+
+        Set<String> originalSet = new HashSet<>();
+        originalSet.add("foo");
+        originalSet.add("buzz");
+        originalSet.add("bar");
+        originalSet.add("fork");
+        originalSet.add("bort");
+        originalSet.add("spoon");
+        originalSet.add("!");
+        originalSet.add("dude");
+
+        System.out.println("Исходное множество: " + originalSet);
+
+        Set<String> filteredSet = remover.removeEvenLength(originalSet);
+
+        System.out.println("Множество после удаления четной длины: " + filteredSet);
+    }
+}
