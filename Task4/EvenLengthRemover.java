6fba2648e03fb6c6af090d60270d9bea04045ba9
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 14:53:43 +0300

    Create EvenLengthRemover.java

---
+++ Task4/EvenLengthRemover.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class EvenLengthRemover {
+
+    public Set<String> removeEvenLength(Set<String> set) {
+        Set<String> resultSet = new HashSet<>();
+
+        for (String str : set) {
+            if (str.length() % 2 != 0) { // Проверка на нечетную длину
+                resultSet.add(str);
+            }
+        }
+
+        return resultSet;
+    }
+}
