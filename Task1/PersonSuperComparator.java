88cc02c82f310d6ed5236a8b0e20cfc8777237e5
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 13:38:06 +0300

    Create PersonSuperComparator.java

---
+++ Task1/PersonSuperComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class PersonSuperComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        // Сравнение по возрасту
+        if (p1.getAge() != p2.getAge()) {
+            return Integer.compare(p1.getAge(), p2.getAge());
+        }
+        // Если возраст одинаковый, сравниваем по имени
+        return p1.getName().compareTo(p2.getName());
+    }
+}
